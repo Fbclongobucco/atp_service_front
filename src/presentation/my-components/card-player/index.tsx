8c5158cf0e2f30cae15ce1bf3c6713751ea9c5bd
longobucco <longobuccodeb@gmail.com>
2025-12-10 17:00:46 -0300

    ajustmente resposivity in card details on generic avatar

--- src/presentation/my-components/card-player/index.tsx
+++ src/presentation/my-components/card-player/index.tsx
@@ -9,9 +9,9 @@ export function CardPlayer({player}: CardPlayerProps) {
     return (
         <div className="w-[97%] h-20 bg-slate-200 flex gap-1 justify-center items-center p-2 
             shadow-md shadow-slate-400 border border-slate-300 rounded-lg hover:bg-slate-300 hover:scale-105 duration-200">
-             <p className="mr-auto font-mono font-semibold text-cyan-800 w-20"># {player.ranking}</p>
+             <p className="mr-auto font-mono font-semibold text-cyan-800 min-w-20"># {player.ranking}</p>
             <div className="w-1/2 flex justify-start items-center">
-                {player.urlPhoto ===  "https://placehold.co/400x400.png" ? <User size={50}color="#302e46" /> :   <img src={player.urlPhoto} alt="logo" width={50} height={50} className="object-cover min-w-[50px]" />}
+                {player.urlPhoto ===  "https://placehold.co/400x400.png" ? <User className="min-w-[50px]" size={50}color="#302e46" /> :   <img src={player.urlPhoto} alt="logo" width={50} height={50} className="object-cover min-w-[50px]" />}
             </div>
             <div className="flex justify-center items-center w-[40%]">
                 <p className="truncate text-slate-600 font-semibold">{player.name}</p>
--- src/presentation/my-components/ranking-list-controll/index.tsx
+++ src/presentation/my-components/ranking-list-controll/index.tsx
@@ -15,7 +15,7 @@ export function RankingListControll({ nextPage, prevPage, reSize, page, size, to
         <div className="flex flex-col gap-2 w-sm md:w-md lg:w-2xl mt-25 mb-2 bg-slate-200 rounded-md">
             <h2 className="font-semibold text-2xl text-slate-600 p-2">ATP <span className="text-lg italic font-bold text-blue-400 tracking-wide">RANKING</span></h2>
             <div className="flex justify-between mx-2 pb-4">
-                <button onClick={prevPage} className="w-10 h-10 rounded-full bg-slate-400 flex items-center justify-center cursor-pointer border border-slate-500 shadow-lg">
+                <button disabled={page === 0}  onClick={prevPage} className="w-10 h-10 rounded-full bg-slate-400 flex items-center justify-center cursor-pointer border border-slate-500 shadow-lg">
 
                     < ArrowLeft size={22} color="#302e46" />
                 </button>
